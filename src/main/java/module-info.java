7516cdd94c9037081d04d7f220f057e67db20999
Verhorubov Mihail <verhorubov.mihail@gmail.com>
2022-07-21 18:01:36 +0700

    Little refactoring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module com.demo.demo {
 
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires jakarta.xml.bind;
+    requires javafx.graphics;
+
     requires com.google.gson;
-    requires java.prefs;
-    requires preferences;
-    requires preferences.xml;
 
 
     opens com.demo.demo to javafx.fxml;
